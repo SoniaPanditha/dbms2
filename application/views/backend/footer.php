ae8339c8896228583c58b6941aafee550535bcc4
DongLee <sriharsha.g15@iiits.in>
2017-03-11 22:13:15 +0530

    Add files via upload

---
+++ application/views/backend/footer.php
@@ -0,0 +1,4 @@
+<footer class="main" style="background:#CCCCCC;padding:5px;text-align:center">
+	<a href="https://www.github.com/SriHarshaGajavalli" 
+    	target=""><strong> Developed by DongLee and Mrinalini</strong>.</a>
+</footer>
